7252a56e1f847410e057b5369cdff26fa867a5de
goodoljosh <findley_joshua1@columbusstate.edu>
2022-09-05 17:26:11 +0000

    Added flask application

--- todo.py
+++ todo.py
@@ -1,3 +1,11 @@
 #app.py 
 
-from flask import flask
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+   return "Hello World!"
+
+if __name__ == "__main__":
+    app.run();
